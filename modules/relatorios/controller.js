b124584f286257782c386fab805ad314a6dad1c2
JulianoHermes <j.hermes09@gmail.com>
2015-11-25 01:57:44 -0200

    intial MVC

--- modules/relatorios/controller.js
+++ modules/relatorios/controller.js
@@ -1,42 +1,18 @@
-var Request = require('tedious').Request;
+
+var model = require('./model');
 
 var Controller = {
     retrieve: function(req,res){
-
-        var Connection = require('tedious').Connection;
-        var connect =new Connection(require('./model'));
-        //var query={_id: req.params.id}
-        //,msg ='';
-        
-
-        request = new Request("select 42, 'hello world'", function(err, rowCount) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(rowCount + ' rows');
-            }
-        });
-
-        request.on('row', function(columns) {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            columns.forEach(function(column) {
-                res.write(column.value+"\n");
+        valores = {};
+        model.selectAll(function(valores){
+            valores.forEach(function(columns) {
+                columns.forEach(function(column) {
+                    console.log(column.metadata.colName + ' = ' + column.value);
+                });
             });
-
-            res.end("Oi");
+            res.render('item', { title: 'Valores', 
+                rows: valores});
         });
-
-        connect.on('connect', function(err) {
-            // If no error, then good to go...
-            if(err){
-                console.log("There Is an error on the force"+err);
-                return;
-            }else{
-                console.log("Successfully connected to the force",connect);
-            }
-            connect.execSql(request);
-        });
-
     }
 }
 
--- modules/relatorios/model.js
+++ modules/relatorios/model.js
@@ -1,22 +1,54 @@
 var Connection = require('tedious').Connection;
-  var config = {
+var Request = require('tedious').Request;
+
+var config = {
     "userName": "root",
     "password": "root",
     "server": "localhost",
+    "database": "sistemarelatorio",
     "options": {
-      //"port":"53235",
-      "encrypt": false
+        //"port":"53235",
+        "encrypt": false,
+        "rowCollectionOnRequestCompletion": true 
     }      
 };
-/*var connection = new Connection(config);
-connection.on('connect', function(err) {
-    // If no error, then good to go...
-    if(err){
-        console.log("There Is an error on the force"+err);
-        return;
-    }else{
-        console.log("Successfully connected to the force",connection);
+
+var connect;
+
+function executeSQL(req){
+    connect = new Connection(config);
+
+    connect.on('connect', function(err) {
+        // If no error, then good to go...
+        if(err){
+            console.log("There Is an error on the force"+err);
+            return;
+        }else{
+            console.log("Successfully connected to the force");
+        }
+        connect.execSql(req);
+    });
+}
+
+
+var model={
+    "selectAll": function (callback){
+        req = new Request("select * from sistemarelatorio.dbo.valores", function(err, rowCount, rows) {
+            if (err) {
+                console.log(err);
+            } else {
+                /*console.log(rowCount + ' rows');
+                rows.forEach(function(columns) {
+                    columns.forEach(function(column) {
+                        console.log(column.metadata.colName + ' = ' + column.value);
+                    });
+                });
+                */
+                callback(rows);
+                connect.close();
+            }
+        });
+        executeSQL(req);
     }
-});
-*/
-module.exports = config;
+};
+module.exports = model;
